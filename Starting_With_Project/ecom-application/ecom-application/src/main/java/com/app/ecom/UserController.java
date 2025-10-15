e3d6405432d7345a4fbf72a8e8b1987abbf6abfe
Vinod Sai <vinodsaikumarllm@gmail.com>
2025-10-15 11:23:08 +0530

    @RequestMapping Annotation

--- Starting_With_Project/ecom-application/ecom-application/src/main/java/com/app/ecom/UserController.java
+++ Starting_With_Project/ecom-application/ecom-application/src/main/java/com/app/ecom/UserController.java
@@ -9,31 +9,33 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/users")
 
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("/api/users")
+    @GetMapping
     public ResponseEntity<List<User>> getAllUsers(){
            // return ResponseEntity.ok(userService.fetchAllUsers()); this will also work
         return new ResponseEntity<>(userService.fetchAllUsers(),
                 HttpStatus.OK);
     }
 
-    @GetMapping("/api/users/{id}")
+   // @RequestMapping(value = "/api/users/{id}", method = RequestMethod.GET)
+    @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@PathVariable Long id){
         return userService.fetchUser(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @PostMapping("/api/users")
+    @PostMapping
     public ResponseEntity<String> createUser(@RequestBody User user){
         userService.addUser(user);
         return ResponseEntity.ok("User Added Succesfully");
     }
 
-    @PutMapping ("/api/users/{id}")
+    @PutMapping ("{id}")
     public ResponseEntity<String> updateUser(@PathVariable Long id,@RequestBody User updatedUser){
        boolean updated  = userService.updateUser(id,updatedUser);
        if(updated)
